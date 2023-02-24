1b540926f9a33984c8d63efaf7fb0d0975fac945
alirida99 <ali_hussein_rida@hotmail.com>
2023-02-24 14:33:00 +0200

    tutorial api

--- src/components/main/user/exam/index.tsx
+++ src/components/main/user/exam/index.tsx
@@ -6,7 +6,8 @@ import router, { useRouter } from 'next/router';
 import {
   Formik,
   Form,
-  Field} from 'formik';
+  Field
+} from 'formik';
 import Footer from '../../footer';
 import UserAppbar from '../appbar';
 /** Exam List + Exam Page */
@@ -14,107 +15,109 @@ import UserAppbar from '../appbar';
 function UsersExam() {
   const router = useRouter()
   const [popUp, setPopUp] = useState(false)
-    
-  
-    return (
-        <>
-        
-        <div  style={{ backgroundImage:  "url(/background.jpg)",
-                    backgroundSize: 'cover',
-                    backgroundRepeat: 'no-repeat',
-                    backgroundAttachment: 'fixed',
-                    backgroundPosition: 'center',
-                    
-                    
-                    }}>
-                      <UserAppbar/>
-    <div className={styles.center}>
-    <div className={styles.tutorialcontainer }>
-
-    <Formik 
-      initialValues={{
-
-        correctanswer: '',
-        useranswer: '',
-        
-
-      }}
-
-        onSubmit={(values, actions) => {
-          console.log({ values, actions });}}
-      
-      >
-        <Form>
-      
-      <h1 className={styles.tutorialtitle}> Exam Title </h1>
 
 
-      {/**MCQ Questions */}
-      <div className={styles.questionbox}> {/**each question is contained inside a questionbox */}
-      <h2 className={styles.questionstatement}>MCQ Question? </h2> {/**Question from database  */}
+  return (
+    <>
 
-        {/**if mcq question  */}
-      <div className={styles.radio} >
-       
-        <label htmlFor="option1">
-            <Field type="radio" name="option" value="option1" />
-            Option 1
-        </label><br/>
-       
-        <label htmlFor="option2">
-            <Field type="radio" name="option" value="option2" />
-            Option 2
-        </label><br/>
-        <label htmlFor="option3">
-            <Field type="radio" name="option" value="option3"/>
-            Option 3
-        </label><br/>
-
-        </div>
-
-      </div> {/** end of questionbox */}
-      <br/><br/>
-      
+      <div style={{
+        backgroundImage: "url(/background.jpg)",
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed',
+        backgroundPosition: 'center',
 
 
-      {/**Drag / Drop Questions */}
+      }}>
+        <UserAppbar />
+        <div className={styles.center}>
+          <div className={styles.tutorialcontainer}>
 
-      <div className={styles.questionbox}>
-        
-      <div className={styles.questionstatement}>
-       
-          <span>Before drag/drop sentence </span>
-          
+            <Formik
+              initialValues={{
 
-          <span>After drag/drop sentence </span>
-       
-      
-      </div>
-      </div>
-      <br/><br/>
+                correctanswer: '',
+                useranswer: '',
 
 
-      {/**Fill in the blank */}
-      <div className={styles.questionbox}>
-        <span className={styles.questionstatement}>First sentence</span>  
-        <Field  className={styles.fillblank} name="name" placeholder=' fill in the blank' />
-        <span className={styles.questionstatement}> second sentence</span>  
-      
-      
-      </div>
+              }}
 
-      <br/><br/>
+              onSubmit={(values, actions) => {
+                console.log({ values, actions });
+              }}
 
-      <button onClick={()=>setPopUp(true)} className={styles.submit} > Submit </button> {/** when pressed popup box of grade + view grades button*/}
-      </Form>
-      </Formik>
-    </div>
-    {popUp && <PopUp setPopUp={setPopUp}/>}
-    <Footer/>
-    </div></div>
-    
+            >
+              <Form>
+
+                <h1 className={styles.tutorialtitle}> Exam Title </h1>
+
+
+                {/**MCQ Questions */}
+                <div className={styles.questionbox}> {/**each question is contained inside a questionbox */}
+                  <h2 className={styles.questionstatement}>MCQ Question? </h2> {/**Question from database  */}
+
+                  {/**if mcq question  */}
+                  <div className={styles.radio} >
+
+                    <label htmlFor="option1">
+                      <Field type="radio" name="option" value="option1" />
+                      Option 1
+                    </label><br />
+
+                    <label htmlFor="option2">
+                      <Field type="radio" name="option" value="option2" />
+                      Option 2
+                    </label><br />
+                    <label htmlFor="option3">
+                      <Field type="radio" name="option" value="option3" />
+                      Option 3
+                    </label><br />
+
+                  </div>
+
+                </div> {/** end of questionbox */}
+                <br /><br />
+
+
+
+                {/**Drag / Drop Questions */}
+
+                <div className={styles.questionbox}>
+
+                  <div className={styles.questionstatement}>
+
+                    <span>Before drag/drop sentence </span>
+
+
+                    <span>After drag/drop sentence </span>
+
+
+                  </div>
+                </div>
+                <br /><br />
+
+
+                {/**Fill in the blank */}
+                <div className={styles.questionbox}>
+                  <span className={styles.questionstatement}>First sentence</span>
+                  <Field className={styles.fillblank} name="name" placeholder=' fill in the blank' />
+                  <span className={styles.questionstatement}> second sentence</span>
+
+
+                </div>
+
+                <br /><br />
+
+                <button onClick={() => setPopUp(true)} className={styles.submit} > Submit </button> {/** when pressed popup box of grade + view grades button*/}
+              </Form>
+            </Formik>
+          </div>
+          {popUp && <PopUp setPopUp={setPopUp} />}
+          <Footer />
+        </div></div>
+
     </>
-    );
+  );
 
 }
 export default UsersExam;
@@ -126,26 +129,26 @@ export default UsersExam;
 
 const PopUp = (props: { setPopUp: any; }) => {
   // function that takes boolean as param to conditionally display popup
-  const { setPopUp } = props 
+  const { setPopUp } = props
 
   return (
-      <div className={styles.PopUp} >
-         
-          
-          <div  className={styles.pucontentcontainer}>
-             
-              <h1>Result:</h1>
-              <h1> 8 of 25</h1> {/**correct answers/total questions */}
-              <h1> 32%</h1> {/**percentage */}
-              <h1> You must study harder!</h1> {/**if high grade : Good Job! */}
-          </div>
-          {/* button controls */}
-          <div className={styles.pubuttoncontainer} >
-              <button className={styles.popupbutton} onClick={() => { router.push('/user/userGrades') }}> View All Grades </button>
-              <button className={styles.popupbutton} onClick={() => { router.push('/user/userExamList') }}>Back to exams</button>
-              
-          </div>
+    <div className={styles.PopUp} >
+
+
+      <div className={styles.pucontentcontainer}>
+
+        <h1>Result:</h1>
+        <h1> 8 of 25</h1> {/**correct answers/total questions */}
+        <h1> 32%</h1> {/**percentage */}
+        <h1> You must study harder!</h1> {/**if high grade : Good Job! */}
       </div>
+      {/* button controls */}
+      <div className={styles.pubuttoncontainer} >
+        <button className={styles.popupbutton} onClick={() => { router.push('/user/userGrades') }}> View All Grades </button>
+        <button className={styles.popupbutton} onClick={() => { router.push('/user/userExamList') }}>Back to exams</button>
+
+      </div>
+    </div>
   );
 }
 
--- src/components/main/user/tutorialList/index.tsx
+++ src/components/main/user/tutorialList/index.tsx
@@ -1,38 +1,85 @@
 
+import { useEffect, useState } from 'react';
 import Footer from '../../footer';
 import UserAppbar from '../appbar';
 import styles from './tutlist.module.scss';
 import { useRouter } from 'next/router';
 
 
-export function UserTutorialList () { //TutorialList 
-  const router = useRouter()
-    return (
-        <>
-        
-        <div  style={{ backgroundImage:  "url(/background.jpg)",
-                    backgroundSize: 'cover',
-                    backgroundRepeat: 'no-repeat',
-                    backgroundAttachment: 'fixed',
-                    backgroundPosition: 'center',
-                    
-                    }}>
-                      <UserAppbar/>
-      <div className={styles.containerlist}>
-      <div className={styles.containergrid}>
-      <button className={styles.buttonlist} onClick={() => { router.push('/user/userTutorial') }}>Tutorial 1</button>
-      <button className={styles.buttonlist}> Tutorial 2</button>
-      <button className={styles.buttonlist}> Tutorial 3</button>
-      <button className={styles.buttonlist}> Tutorial 4</button>
-      <button className={styles.buttonlist}> Tutorial 5</button>
-      <button className={styles.buttonlist}>Tutorial 6</button>
+export function UserTutorialList() { //TutorialList 
+  const router = useRouter();
+  const [tutorials, setTutorials] = useState([] as any);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
-      </div>
-   
-    </div>
-    <Footer/></div>
-   
+  useEffect(() => {
+    const fetchTutorials = async () => {
+      const response = await fetch('https://capstone-final-adf33-default-rtdb.firebaseio.com/tutorials.json');
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!!')
+      }
+
+      const responseData = await response.json();
+
+      const loadedTutorials = [];
+
+      for (const key in responseData) {
+        loadedTutorials.push({
+          id: key,
+          title: responseData[key].title,
+          author: responseData[key].author,
+          creatingDate: responseData[key].creatingDate,
+          topic: responseData[key].topic,
+        });
+      }
+
+      setTutorials(loadedTutorials);
+      setIsLoading(false);
+    }
+    fetchTutorials().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message)
+    });
+  }, []);
+
+  return (
+    <>
+
+      <div style={{
+        backgroundImage: "url(/background.jpg)",
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundAttachment: 'fixed',
+        backgroundPosition: 'center',
+
+      }}>
+        <UserAppbar />
+        <div className={styles.containerlist}>
+          {!isLoading && !httpError &&
+            <div className={styles.containergrid}>
+              {tutorials && tutorials.map((tutorial: any) => {
+                return (
+                  <button
+                    key={tutorial.id}
+                    className={styles.buttonlist}
+                    onClick={() => {
+                      router.push({
+                        pathname: '/user/userTutorial',
+                        query: {
+                          id: tutorial.id,
+                        }
+                      })
+                    }}>{tutorial.title}</button>
+                )
+              })}
+            </div>
+          }
+
+        </div>
+        <Footer /></div>
+
     </>
-  
-    );
-  }
+
+  );
+}
